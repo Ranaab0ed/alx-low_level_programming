d353ef722b213c1e2b7039e89be3d3458aca9f7d
rana <rabed1573@gmail.com>
2023-09-07 13:34:26 -0700

    t

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf ("size of a"sizeof(int));
+	printf ("size of a"sizeof(char));
+	printf ("size of a"sizeof(long long));
+	printf ("size of a"sizeof(long));
+	printf ("size of a"sizeof(float));
+	return 0 ;
+}
