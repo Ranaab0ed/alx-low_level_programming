173ecf003ad6e5b68f993d5d9aea9f414bda67cf
rana <rabed1573@gmail.com>
2023-09-12 13:20:29 -0700

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -entry point 
+ * describtion; prints _putchar using puchar prototype
+ * return :always 0(succes)
+ *
+ */
+int main (void)
+	char str[] "_putchar";
+	int ch ;
+	for (ch=0;ch<8,ch++);
+	_putchar (str[ch]);
+	_putchar ('\n');
+	return 0 ;
